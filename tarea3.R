57e8b1df542a470a9a05a0e7a41806a099f72916
deyban.perez <deyban.perez@gmail.com>
2016-03-29 19:52:21 -0430

    no message

--- tarea3.R
+++ tarea3.R
@@ -129,59 +129,7 @@ plot(df_a_big[, 1:2], col = model_kmeans$cluster,
      main = "Data set a_big.csv", sub = "K-meas algorithm",
      xlab = "Feature 1", ylab = "Feature 2")
 points(model_kmeans$centers[,c("V1", "V2")],
-       col = 7:9,
+       col = 6:8,
        pch = 19,
        cex = 2)
 table(True = df_a_big$V3, Prediction = model_kmeans$cluster)
-############
-#H-Clusters#
-############
-input_hierarchical = df_a
-input_hierarchical_classes = as.numeric(df_a$V3)
-
-
-for (i in 1:length(input_hierarchical_classes))
-{
-  if(input_hierarchical_classes[i] == 1)
-    input_hierarchical_classes[i] = 0
-}
-
-for (i in 1:length(input_hierarchical_classes))
-{
-  if(input_hierarchical_classes[i] == 2)
-    input_hierarchical_classes[i] = 1
-}
-
-for (i in 1:length(input_hierarchical_classes))
-{
-  if(input_hierarchical_classes[i] == 0)
-    input_hierarchical_classes[i] = 2
-}
-
-input_hierarchical$V3 = NULL
-input_hierarchical = as.matrix(input_hierarchical)
-hierarchical_distance = dist(input_hierarchical)
-###############
-#Single method#
-###############
-model_hierarchical_single = hclust(hierarchical_distance, method = "single")
-plot(model_hierarchical_single)
-model_hierarchical_single_cut = cutree(model_hierarchical_single, k = 3)
-plot(x = df_a$V1, y = df_a$V2, col = model_hierarchical_single_cut)
-table(True = input_hierarchical_classes, Predicted = model_hierarchical_single_cut)
-#################
-#Complete method#
-#################
-model_hierarchical_complete = hclust(hierarchical_distance, method = "complete")
-plot(model_hierarchical_complete)
-model_hierarchical_complete_cut = cutree(model_hierarchical_complete, k = 3)
-plot(x = df_a$V1, y = df_a$V2, col = model_hierarchical_complete_cut)
-table(True = input_hierarchical_classes, Predicted = model_hierarchical_complete_cut)
-#################
-#Average method#
-#################
-model_hierarchical_average = hclust(hierarchical_distance, method = "average")
-plot(model_hierarchical_average)
-model_hierarchical_average_cut = cutree(model_hierarchical_average, k = 3)
-plot(x = df_a$V1, y = df_a$V2, col = model_hierarchical_average_cut)
-table(True = input_hierarchical_classes, Predicted = model_hierarchical_average_cut)
